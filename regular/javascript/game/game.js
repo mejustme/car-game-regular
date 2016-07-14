9ee4ceeb3714222e67d43a7f043ef720e4b0bed8
陈钦辉 <hzchenqinhui@corp.netease.com>
2016-07-14 15:06:41 +0800

    @dev road ok

---
+++ regular/javascript/game/game.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cqh on 16/7/14.
+ * ------------------------------------------
+ * 窗体控件实现文件
+ * @version  1.0
+ * @author   陈钦辉(hzchenqinhui@corp.netease.com)
+ * @path  pro/game/game
+ * ------------------------------------------
+ */
+
+NEJ.define(['regular!./game.html'],function(tpl){
+    var Game =  Regular.extend({
+        name: 'game',
+        template: tpl,
+        data: {}
+    });
+    return Game;
+});
---
+++ regular/javascript/road/road.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cqh on 16/7/14.
+ * ------------------------------------------
+ * 窗体控件实现文件
+ * @version  1.0
+ * @author   陈钦辉(hzchenqinhui@corp.netease.com)
+ * @path  pro/org/ui/editClass/classAttach
+ * ------------------------------------------
+ */
